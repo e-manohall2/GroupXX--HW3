bed56d4bb1e817b3954d2e9fa866c6ab8ea7df97
e-manohall2 <eamonn.ohalloran@icloud.com>
2026-09-24 18:19:55 -0500

    Initial address commit

--- address.cpp
+++ address.cpp
@@ -1,1 +1,14 @@
+#include "address.h"
+#include <string>
+using std::string;
+
+
+string address::address_info(){
+    string ret = "Address:\n"+address_line1+"\n"+address_line2+"\n"+city+"\n"+state+"\n"+zip;
+    return ret;
+}
+
+
 //implementation code for address struct here
+
+
